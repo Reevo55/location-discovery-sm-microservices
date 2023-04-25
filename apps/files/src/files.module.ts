b61097712e07cec931343426af2e7058ea1935f0
Radek <r.karbowiak@brainforgeit.com>
2023-04-25 12:45:32 +0200

    rm: console .log

--- apps/files/src/files.module.ts
+++ apps/files/src/files.module.ts
@@ -3,9 +3,22 @@ import { ConfigModule } from '@nestjs/config';
 
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        AWS_REGION: Joi.string().required(),
+        AWS_ACCESS_KEY_ID: Joi.string().required(),
+        AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+        AWS_PUBLIC_BUCKET_NAME: Joi.string().required(),
+        AWS_S3_SIGNED_URL_EXPIRATION: Joi.number().required(),
+      }),
+      envFilePath: './apps/files/.env',
+    }),
+  ],
   controllers: [FilesController],
   providers: [FilesService],
 })
--- apps/files/src/files.service.ts
+++ apps/files/src/files.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { S3 } from 'aws-sdk';
+import {
+  S3Client,
+  PutObjectCommand,
+  DeleteObjectCommand,
+} from '@aws-sdk/client-s3';
+import { createRequest } from '@aws-sdk/util-create-request';
+import { formatUrl } from '@aws-sdk/util-format-url';
+import { S3RequestPresigner } from '@aws-sdk/s3-request-presigner';
 import { v4 as uuid } from 'uuid';
 
 @Injectable()
@@ -14,39 +21,48 @@ export class FilesService {
       throw new Error('No content type');
     }
 
-    const s3 = this.getS3Object();
-
+    const s3Client = this.getS3Client();
     const key = `files/${uuid()}.${contentType}`;
 
-    const parameters = {
+    const params = {
       Bucket: this.configService.get('AWS_PUBLIC_BUCKET_NAME'),
       Key: key,
       ContentType: contentType,
-      Expires: Number.parseInt(process.env.AWS_S3_SIGNED_URL_EXPIRATION) || 60,
     };
 
-    const url = s3.getSignedUrl('putObject', parameters);
+    const request = await createRequest(s3Client, new PutObjectCommand(params));
+    const presigner = new S3RequestPresigner({ ...s3Client.config });
+    const expiration =
+      Number.parseInt(process.env.AWS_S3_SIGNED_URL_EXPIRATION) || 60;
 
-    return { url, key };
+    const url = formatUrl(
+      await presigner.presign(request, { expiresIn: expiration }),
+    );
+
+    return {
+      url,
+      key,
+    };
   }
 
   async deleteFileFromS3(url: string) {
-    const s3 = this.getS3Object();
-
+    const s3Client = this.getS3Client();
     const key = url.split('.com/')[1];
 
-    const parameters = {
+    const params = {
       Bucket: this.configService.get('AWS_PUBLIC_BUCKET_NAME'),
       Key: key,
     };
 
-    return await s3.deleteObject(parameters).promise();
+    await s3Client.send(new DeleteObjectCommand(params));
   }
 
-  private getS3Object() {
-    return new S3({
-      accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+  private getS3Client() {
+    return new S3Client({
+      credentials: {
+        accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      },
       region: this.configService.get('AWS_REGION'),
     });
   }
